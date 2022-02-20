a0a0122a32df8a67f4417145c7a21a5f8f192f28
DaniilPetrov <dany_petrov@icloud.com>
2022-02-20 15:43:00 +0300

    Initial Commit

---
+++ My Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Places
+//
+//  Created by Даниил Петров on 20.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
